2ec46e578f69db64c7ad29062d89f5643b5d4430
Drake Dwornik <drake@drakedwornik.com>
2021-11-23 23:07:55 -0500

    working profile view (in the abstract) added requirements, added an error template

--- forum/app.py
+++ forum/app.py
@@ -3,9 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 app.config.update(
     TESTING=True,
-	DEBUG=True,
+    DEBUG=True,
     SECRET_KEY=b'kristofer',
-	SITE_NAME = "CHYRP",
-	SITE_DESCRIPTION = "CHYRP Has You Reading Posts",
-	SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db'
+    SITE_NAME="CHYRP",
+    SITE_DESCRIPTION="CHYRP Has You Reading Posts",
+    SQLALCHEMY_DATABASE_URI='sqlite:////tmp/database.db',
+    IMAGES_PATH=['images']
 )
