09352ff3a9dc3f3eec92038c9359d9d6dfe88caa
Drake Dwornik <drake@drakedwornik.com>
2021-12-13 15:02:25 -0500

    works on postgres

--- forum/app.py
+++ forum/app.py
@@ -5,11 +5,10 @@ app.config.update(
     TESTING=True,
     DEBUG=True,
     SECRET_KEY=b'kristofer',
-    SQLALCHEMY_DATABASE_URI='sqlite:///tmp/database.db',
     COPY_RIGHT="Copyright © 2021 CHYRP. All rights reserved",
     SITE_NAME="CHYRP",
     SITE_DESCRIPTION="CHYRP Has You Reading Posts",
     IMAGES_PATH=['images'],
-    SQLALCHEMY_TRACK_MODIFICATIONS=True,
-    SESSION_TYPE="filesystem"
+    SQLALCHEMY_DATABASE_URI='postgresql://ccuser:foobar@localhost/circuscircus',
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
 )
